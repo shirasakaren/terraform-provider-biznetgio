dc01a2e7269bdd572fc881fe00bd4aad9359d821
Shirasaka Ren <ren@shirasaka.ren>
2026-05-06 17:50:00 +0700

    docstring rapi baremetal_additional_ip.go cok

---
+++ internal/provider/baremetal_additional_ip.go
@@ -0,0 +1,84 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/biznetgio/terraform-provider-biznetgio/internal/biznetgio"
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type BaremetalAdditionalIPResource struct {
+	client *biznetgio.Client
+}
+
+type BaremetalAdditionalIPResourceModel struct {
+	ID                types.String   `tfsdk:"id"`
+	AccountID         types.Int64    `tfsdk:"account_id"`
+	ProductID         types.Int64    `tfsdk:"product_id"`
+	Cycle             types.String   `tfsdk:"cycle"`
+	Region            types.String   `tfsdk:"region"`
+	Promocode         types.String   `tfsdk:"promocode"`
+	PayWithCreditCard types.Bool     `tfsdk:"pay_with_credit_card"`
+	Status            types.String   `tfsdk:"status"`
+	IPAddress         types.String   `tfsdk:"ip_address"`
+	CreatedAt         types.String   `tfsdk:"created_at"`
+	Raw               types.String   `tfsdk:"raw"`
+	Timeouts          timeouts.Value `tfsdk:"timeouts"`
+}
+
+func NewBaremetalAdditionalIPResource() resource.Resource {
+	return &BaremetalAdditionalIPResource{}
+}
+
+func (r *BaremetalAdditionalIPResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_baremetal_additional_ip"
+}
+
+func (r *BaremetalAdditionalIPResource) Schema(ctx context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Additional (floating) IP untuk NEO Metal. Diorder independen dari server, lalu di-attach pake `biznetgio_baremetal_additional_ip_assignment`.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Resource id = account_id IP.",
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
+			},
+			"account_id": schema.Int64Attribute{
+				Computed:            true,
+				MarkdownDescription: "Account id additional IP di BiznetGIO.",
+				PlanModifiers:       []planmodifier.Int64{int64planmodifier.UseStateForUnknown()},
+			},
+			"product_id": schema.Int64Attribute{
+				Required:            true,
+				MarkdownDescription: "Product id dari `GET /baremetal-additional-ips/products`.",
+				PlanModifiers:       []planmodifier.Int64{int64planmodifier.RequiresReplace()},
+			},
+			"cycle": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "Siklus billing, misal `m` (monthly) atau `a` (annual).",
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.RequiresReplace()},
+			},
+			"region": schema.StringAttribute{
+				Optional:            true,
+				Computed:            true,
+				Default:             stringdefault.StaticString("wjv-1"),
+				MarkdownDescription: "Region datacenter, list valid dari `GET /baremetal-additional-ips/regions`. Default `wjv-1`.",
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.RequiresReplace()},
+			},
+			"promocode": schema.StringAttribute{
+				Optional:            true,
+				MarkdownDescription: "Kode promo.",
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.RequiresReplace()},
+			},
