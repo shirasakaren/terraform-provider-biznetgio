6582ae1404da9a669bebe93d4cd6df50e102f622
Shirasaka Ren <ren@shirasaka.ren>
2026-05-06 23:15:00 +0700

    tambah neolitepro_disk.go: trigger attribute wkwk

--- internal/provider/neolitepro_disk.go
+++ internal/provider/neolitepro_disk.go
@@ -1,1 +1,45 @@
-// wip 0
+package provider
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/biznetgio/terraform-provider-biznetgio/internal/biznetgio"
+	"github.com/hashicorp/terraform-plugin-framework-timeouts/resource/timeouts"
+	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"regexp"
+)
+
+type NeoliteProDiskResource struct {
+	client *biznetgio.Client
+}
+
+type NeoliteProDiskResourceModel struct {
+	ID                types.String   `tfsdk:"id"`
+	OrderID           types.String   `tfsdk:"order_id"`
+	ProductID         types.Int64    `tfsdk:"product_id"`
+	Cycle             types.String   `tfsdk:"cycle"`
+	NeoliteAccountID  types.Int64    `tfsdk:"neolite_account_id"`
+	ServiceName       types.String   `tfsdk:"service_name"`
+	Promocode         types.String   `tfsdk:"promocode"`
+	PayWithCreditCard types.Bool     `tfsdk:"pay_with_credit_card"`
+	Size              types.Int64    `tfsdk:"size"`
+	Status            types.String   `tfsdk:"status"`
+	Raw               types.String   `tfsdk:"raw"`
+	Timeouts          timeouts.Value `tfsdk:"timeouts"`
+}
+
