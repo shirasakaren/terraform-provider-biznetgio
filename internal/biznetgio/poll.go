a964d319a5b6e0ee5ce956bb418d4e4fa08b2650
Shirasaka Ren <ren@shirasaka.ren>
2026-05-15 17:11:00 +0700

    tambah poll.go: docstring rapi gas

--- internal/biznetgio/poll.go
+++ internal/biznetgio/poll.go
@@ -1,1 +1,31 @@
-// wip 0
+package biznetgio
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+const retryAttempts = 3
+
+func retryable(err error) bool {
+	var apiErr *APIError
+	if !errors.As(err, &apiErr) {
+		return false
+	}
+	switch apiErr.StatusCode {
+	case http.StatusTooManyRequests, http.StatusInternalServerError,
+		http.StatusBadGateway, http.StatusServiceUnavailable, http.StatusGatewayTimeout:
+		return true
+	}
+	return false
+}
+
+func retryBackoff(attempt int) time.Duration {
+	switch attempt {
+	case 1:
+		return 500 * time.Millisecond
+	case 2:
+		return time.Second
