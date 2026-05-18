fbb26c6e2a2e3e66b8070217a408c3ee9900e29e
Shirasaka Ren <ren@shirasaka.ren>
2026-05-18 16:36:00 +0700

    tambah resource.tf: polling biar nunggu active gas

--- examples/resources/biznetgio_gpu_instance/resource.tf
+++ examples/resources/biznetgio_gpu_instance/resource.tf
@@ -1,1 +1,19 @@
-# wip 0
+provider "biznetgio" {}
+
+resource "biznetgio_gpu_instance" "example" {
+  product_id           = 123
+  select_os            = "ubuntu-22"
+  keypair_id           = 456
+  service_name         = "neo-gpu-biznet"
+  ssh_and_console_user = "root"
+  console_password     = "ganti-password-ini"
+  pay_with_credit_card = true
+
+  subscription {
+    cycle = "m"
+  }
+
+  # ubah value ini buat rebuild ulang (destructive) atau reserve jam tambahan
+  rebuild_trigger                  = "init"
+  reserve_additional_hours_trigger = ""
+}
