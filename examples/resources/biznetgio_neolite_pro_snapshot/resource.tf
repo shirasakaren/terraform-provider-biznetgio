a31a51ed39048e0c1dcb093b6eb3c9ce89bddcd0
Shirasaka Ren <ren@shirasaka.ren>
2026-05-26 17:58:00 +0700

    tambah resource.tf: crud dasar cok

--- examples/resources/biznetgio_neolite_pro_snapshot/resource.tf
+++ examples/resources/biznetgio_neolite_pro_snapshot/resource.tf
@@ -1,1 +1,7 @@
-# wip 0
+# Snapshot akun VM NEO Lite Pro (neolite_account_id = id dari biznetgio_neolite_pro_vm).
+resource "biznetgio_neolite_pro_snapshot" "main" {
+  neolite_account_id = biznetgio_neolite_pro_vm.main.id
+  name               = "before-upgrade"
+  description        = "Snapshot sebelum upgrade package"
+  cycle              = "m"
+}
