e7e96ab2b966472354a4d31272cf10027a082074
Shirasaka Ren <ren@shirasaka.ren>
2026-05-26 11:26:00 +0700

    tambah data-source.tf: sensitive fields

--- examples/data-sources/biznetgio_neolite_pro_change_package_options/data-source.tf
+++ examples/data-sources/biznetgio_neolite_pro_change_package_options/data-source.tf
@@ -1,1 +1,8 @@
-# wip 0
+# Ganti <account_id> dengan account id VM NEO Lite Pro.
+data "biznetgio_neolite_pro_change_package_options" "this" {
+  account_id = 12345
+}
+
+output "options_raw" {
+  value = data.biznetgio_neolite_pro_change_package_options.this.raw
+}
