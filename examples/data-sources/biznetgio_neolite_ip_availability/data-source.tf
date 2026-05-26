6dd44d45fef851e5f2c1992320bfd096483eb7c7
Shirasaka Ren <ren@shirasaka.ren>
2026-05-26 23:59:00 +0700

    tambah data-source.tf: import state

--- examples/data-sources/biznetgio_neolite_ip_availability/data-source.tf
+++ examples/data-sources/biznetgio_neolite_ip_availability/data-source.tf
@@ -1,3 +1,9 @@
-# wip 0
-# wip 363
-# wip 392
+data "biznetgio_neolite_products" "all" {}
+
+data "biznetgio_neolite_ip_availability" "this" {
+  product_id = data.biznetgio_neolite_products.all.products[0].product_id
+}
+
+output "available" {
+  value = data.biznetgio_neolite_ip_availability.this.available
+}
