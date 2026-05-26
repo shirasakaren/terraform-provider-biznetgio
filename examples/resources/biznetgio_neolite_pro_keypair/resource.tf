78e1585a13ddc9ac93c74695bec9e845f5410ab8
Shirasaka Ren <ren@shirasaka.ren>
2026-05-26 09:00:00 +0700

    tambah resource.tf: struktur awal

--- examples/resources/biznetgio_neolite_pro_keypair/resource.tf
+++ examples/resources/biznetgio_neolite_pro_keypair/resource.tf
@@ -1,1 +1,10 @@
-# wip 0
+# Keypair di-generate server; private_key cuma keluar sekali di state.
+resource "biznetgio_neolite_pro_keypair" "main" {
+  name = "neo-lite-pro-key"
+}
+
+# Simpen private key-nya, misal pake output sensitive:
+output "private_key" {
+  value     = biznetgio_neolite_pro_keypair.main.private_key
+  sensitive = true
+}
