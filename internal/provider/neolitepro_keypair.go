4433f816170c88b75afc8535d4a1e644e04d270f
Shirasaka Ren <ren@shirasaka.ren>
2026-05-30 16:07:00 +0700

    schema neolitepro_keypair.go cok

--- internal/provider/neolitepro_keypair.go
+++ internal/provider/neolitepro_keypair.go
@@ -1,3 +1,86 @@
-// wip 0
-// wip 109
-// wip 235
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/biznetgio/terraform-provider-biznetgio/internal/biznetgio"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type NeoliteProKeypairResource struct {
+	client *biznetgio.Client
+}
+
+type NeoliteProKeypairResourceModel struct {
+	ID         types.String `tfsdk:"id"`
+	KeypairID  types.Int64  `tfsdk:"keypair_id"`
+	Name       types.String `tfsdk:"name"`
+	PublicKey  types.String `tfsdk:"public_key"`
+	PrivateKey types.String `tfsdk:"private_key"`
+}
+
+func NewNeoliteProKeypairResource() resource.Resource {
+	return &NeoliteProKeypairResource{}
+}
+
+func (r *NeoliteProKeypairResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_neolite_pro_keypair"
+}
+
+func (r *NeoliteProKeypairResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "SSH keypair untuk NEO Lite Pro (pool keypair neolite-pro, terpisah dari baremetal/gpu). Keypair di-generate server; private key cuma keluar sekali di response create.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Resource id = keypair_id.",
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
+			},
+			"keypair_id": schema.Int64Attribute{
+				Computed:            true,
+				MarkdownDescription: "Id keypair di BiznetGIO.",
+				PlanModifiers:       []planmodifier.Int64{int64planmodifier.UseStateForUnknown()},
+			},
+			"name": schema.StringAttribute{
+				Required:            true,
+				MarkdownDescription: "Nama keypair.",
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.RequiresReplace()},
+			},
+			"public_key": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Public key yang di-generate.",
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
+			},
+			"private_key": schema.StringAttribute{
+				Computed:            true,
+				Sensitive:           true,
+				MarkdownDescription: "Private key (sensitive). Write-only: cuma ada di response create, ga bisa di-refetch.",
+				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
+			},
+		},
+	}
+}
+
+func (r *NeoliteProKeypairResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+	client, ok := req.ProviderData.(*biznetgio.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected *biznetgio.Client, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+		return
+	}
+	r.client = client
+}
+
