b10cffd6e34799352c58b5daa6dbe65a0db5e565
Shirasaka Ren <ren@shirasaka.ren>
2026-05-31 15:29:00 +0700

    redact raw baremetal_acceptance_test.go

--- internal/provider/baremetal_acceptance_test.go
+++ internal/provider/baremetal_acceptance_test.go
@@ -1,1 +1,70 @@
-// wip 0
+package provider
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/biznetgio/terraform-provider-biznetgio/internal/biznetgio"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+)
+
+func testAccClient() *biznetgio.Client {
+	return biznetgio.New(os.Getenv("BIZNETGIO_BASE_URL"), os.Getenv("BIZNETGIO_API_KEY"), 30*time.Second)
+}
+
+func testAccCheckBaremetalDestroy(s *terraform.State) error {
+	client := testAccClient()
+	items, err := client.Baremetal().AccountsList(context.Background(), "")
+	if err != nil {
+		return err
+	}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "biznetgio_baremetal" {
+			continue
+		}
+		id, err := strconv.ParseInt(rs.Primary.ID, 10, 64)
+		if err != nil {
+			return err
+		}
+		for _, it := range items {
+			if aliasInt(it, "account_id", "id") == id {
+				return fmt.Errorf("baremetal %d masih ada setelah destroy", id)
+			}
+		}
+	}
+	return nil
+}
+
+func testAccCheckBaremetalKeypairDestroy(s *terraform.State) error {
+	client := testAccClient()
+	items, err := client.Baremetal().KeypairList(context.Background())
+	if err != nil {
+		return err
+	}
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "biznetgio_baremetal_keypair" {
+			continue
+		}
+		id, err := strconv.ParseInt(rs.Primary.ID, 10, 64)
+		if err != nil {
+			return err
+		}
+		for _, it := range items {
+			if aliasInt(it, "keypair_id", "id") == id {
+				return fmt.Errorf("baremetal keypair %d masih ada setelah destroy", id)
+			}
+		}
+	}
+	return nil
+}
+
+func TestAccBaremetal_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		PreCheck:                 func() { testAccPreCheck(t) },
+		CheckDestroy:             testAccCheckBaremetalDestroy,
