a52e1d57e0c8de142b6b83061ce368068e25ac08
Shirasaka Ren <ren@shirasaka.ren>
2026-06-01 16:09:00 +0700

    tambah resource.tf: hash id biar aman

--- examples/resources/biznetgio_object_storage/resource.tf
+++ examples/resources/biznetgio_object_storage/resource.tf
@@ -1,1 +1,8 @@
-# wip 0
+# Subscribed Object Storage instance. Polls until Active before proceeding.
+resource "biznetgio_object_storage" "example" {
+  product_id           = "8"
+  cycle                = "m"
+  label                = "data-archive"
+  quota                = 10
+  pay_with_credit_card = true
+}
