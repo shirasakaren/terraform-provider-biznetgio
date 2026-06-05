fa2010c82748dfa5dd454f0ba3c149dc08474dfa
Shirasaka Ren <ren@shirasaka.ren>
2026-06-05 09:21:00 +0700

    validasi input gpu_data_sources.go

--- internal/provider/gpu_data_sources.go
+++ internal/provider/gpu_data_sources.go
@@ -1,2 +1,129 @@
-// wip 0
-// wip 198
+// SPDX-License-Identifier: MPL-2.0
+
+package provider
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/biznetgio/terraform-provider-biznetgio/internal/biznetgio"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+var _ datasource.DataSource = &GpuProductsDataSource{}
+
+type GpuProductsDataSource struct {
+	client *biznetgio.Client
+}
+
+type GpuProductsDataSourceModel struct {
+	ID       types.String `tfsdk:"id"`
+	Products types.List   `tfsdk:"products"`
+}
+
+type GpuProductModel struct {
+	ProductID    types.Int64  `tfsdk:"product_id"`
+	Name         types.String `tfsdk:"name"`
+	Description  types.String `tfsdk:"description"`
+	CategoryName types.String `tfsdk:"category_name"`
+	Raw          types.String `tfsdk:"raw"`
+	Flavors      types.List   `tfsdk:"flavors"`
+}
+
+type GpuFlavorModel struct {
+	FlavorID types.Int64  `tfsdk:"flavor_id"`
+	Name     types.String `tfsdk:"name"`
+	Raw      types.String `tfsdk:"raw"`
+}
+
+func gpuProductAttrTypes() map[string]attr.Type {
+	return map[string]attr.Type{
+		"product_id":    types.Int64Type,
+		"name":          types.StringType,
+		"description":   types.StringType,
+		"category_name": types.StringType,
+		"raw":           types.StringType,
+		"flavors":       types.ListType{ElemType: types.ObjectType{AttrTypes: gpuFlavorAttrTypes()}},
+	}
+}
+
+func gpuFlavorAttrTypes() map[string]attr.Type {
+	return map[string]attr.Type{
+		"flavor_id": types.Int64Type,
+		"name":      types.StringType,
+		"raw":       types.StringType,
+	}
+}
+
+func (d *GpuProductsDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = "biznetgio_gpu_products"
+}
+
+func (d *GpuProductsDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "GPU product catalog, including available flavors per product.",
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed:            true,
+				MarkdownDescription: "Static identifier of the data source.",
+			},
+			"products": schema.ListNestedAttribute{
+				Computed: true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"product_id": schema.Int64Attribute{
+							Computed:            true,
+							MarkdownDescription: "Product id, orderable in `biznetgio_gpu_instance`.",
+						},
+						"name": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "Product name.",
+						},
+						"description": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "Product description.",
+						},
+						"category_name": schema.StringAttribute{
+							Computed:            true,
+							MarkdownDescription: "Product category.",
+						},
+						"raw": schema.StringAttribute{
+							Sensitive:           true,
+							Computed:            true,
+							MarkdownDescription: "Raw JSON of the product, for anything not modeled yet.",
+						},
+						"flavors": schema.ListNestedAttribute{
+							Computed: true,
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"flavor_id": schema.Int64Attribute{
+										Computed:            true,
+										MarkdownDescription: "Flavor id.",
+									},
+									"name": schema.StringAttribute{
+										Computed:            true,
+										MarkdownDescription: "Flavor name.",
+									},
+									"raw": schema.StringAttribute{
+										Sensitive:           true,
+										Computed:            true,
+										MarkdownDescription: "Raw JSON of the flavor.",
+									},
+								},
+							},
+							MarkdownDescription: "Available flavors of the product.",
+						},
+					},
+				},
+				MarkdownDescription: "List of GPU products.",
+			},
+		},
+	}
+}
+
+func (d *GpuProductsDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
