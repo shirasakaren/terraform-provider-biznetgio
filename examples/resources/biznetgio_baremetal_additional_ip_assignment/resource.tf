4a7de3c67818729df2cd7d55412e92c7d11c1fe0
Shirasaka Ren <ren@shirasaka.ren>
2026-06-05 12:54:00 +0700

    tambah resource.tf: planmodifier

---
+++ examples/resources/biznetgio_baremetal_additional_ip_assignment/resource.tf
@@ -0,0 +1,17 @@
+resource "biznetgio_baremetal_additional_ip" "main" {
+  product_id = 1
+  cycle      = "m"
+}
+
+resource "biznetgio_baremetal" "main" {
+  product_id = 1
+  cycle      = "m"
+  label      = "neo-metal-main"
+  keypair_id = 1
+  public_ip  = 1
+}
+
+resource "biznetgio_baremetal_additional_ip_assignment" "main" {
+  additional_ip_id = biznetgio_baremetal_additional_ip.main.account_id
+  metal_account_id = biznetgio_baremetal.main.account_id
+}
