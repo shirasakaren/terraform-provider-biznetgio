a08738fc99adf3e9afe494e4d68b3957c91aa4e1
Shirasaka Ren <ren@shirasaka.ren>
2026-06-05 15:11:00 +0700

    tambah data-source.tf: satu arah migrate bjir

--- examples/data-sources/biznetgio_neolite_change_package_options/data-source.tf
+++ examples/data-sources/biznetgio_neolite_change_package_options/data-source.tf
@@ -1,2 +1,8 @@
-# wip 0
-# wip 79
+# Ganti <account_id> dengan account id VM NEO Lite.
+data "biznetgio_neolite_change_package_options" "this" {
+  account_id = 12345
+}
+
+output "options_raw" {
+  value = data.biznetgio_neolite_change_package_options.this.raw
+}
