9fef0a4da9fe5158600615dc40e34303468b9fd0
Shirasaka Ren <ren@shirasaka.ren>
2026-06-06 23:14:00 +0700

    tambah data-source.tf: struktur awal gas

--- examples/data-sources/biznetgio_neolite_os_list/data-source.tf
+++ examples/data-sources/biznetgio_neolite_os_list/data-source.tf
@@ -1,4 +1,9 @@
-# wip 0
-# wip 59
-# wip 141
-# wip 347
+data "biznetgio_neolite_products" "all" {}
+
+data "biznetgio_neolite_os_list" "this" {
+  product_id = data.biznetgio_neolite_products.all.products[0].product_id
+}
+
+output "os_names" {
+  value = [for os in data.biznetgio_neolite_os_list.this.oss : os.name]
+}
