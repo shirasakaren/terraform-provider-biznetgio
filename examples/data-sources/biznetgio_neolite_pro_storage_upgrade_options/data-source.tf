aca60ed7d371a69eec1307afb28d41cf3cd36bed
Shirasaka Ren <ren@shirasaka.ren>
2026-06-07 19:19:00 +0700

    tambah data-source.tf: hash id biar aman cok

--- examples/data-sources/biznetgio_neolite_pro_storage_upgrade_options/data-source.tf
+++ examples/data-sources/biznetgio_neolite_pro_storage_upgrade_options/data-source.tf
@@ -1,1 +1,8 @@
-# wip 0
+# Ganti <account_id> dengan account id VM NEO Lite Pro.
+data "biznetgio_neolite_pro_storage_upgrade_options" "this" {
+  account_id = 12345
+}
+
+output "options_raw" {
+  value = data.biznetgio_neolite_pro_storage_upgrade_options.this.raw
+}
