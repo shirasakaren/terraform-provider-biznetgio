0576108f772d65c67eb13d8f5254ccd1304f105d
Shirasaka Ren <ren@shirasaka.ren>
2026-06-07 19:49:00 +0700

    tambah resource.tf: error handling hehe

--- examples/resources/biznetgio_neolite_keypair/resource.tf
+++ examples/resources/biznetgio_neolite_keypair/resource.tf
@@ -1,1 +1,9 @@
-# wip 0
+# Keypair di-generate server; private key cuma keluar sekali di response create.
+resource "biznetgio_neolite_keypair" "main" {
+  name = "neo-lite-key"
+}
+
+# Import keypair yang sudah ada:
+# resource "biznetgio_neolite_keypair" "imported" {
+#   name = "existing-key"
+# }
