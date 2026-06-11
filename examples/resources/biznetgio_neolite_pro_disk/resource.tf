7287cfc79977e4bed052d663aa3511fab70edc92
Shirasaka Ren <ren@shirasaka.ren>
2026-06-11 13:05:00 +0700

    tambah resource.tf: docstring rapi

--- examples/resources/biznetgio_neolite_pro_disk/resource.tf
+++ examples/resources/biznetgio_neolite_pro_disk/resource.tf
@@ -1,2 +1,10 @@
-# wip 0
-# wip 478
+# Disk tambahan NEO Lite Pro (neolite_account_id = id dari biznetgio_neolite_pro_vm).
+resource "biznetgio_neolite_pro_disk" "main" {
+  product_id         = 30
+  cycle              = "m"
+  neolite_account_id = biznetgio_neolite_pro_vm.main.id
+  service_name       = "extra-data"
+  size               = 30
+}
+
+# Upgrade disk: naikin size (increment otomatis dihitung dari selisih).
