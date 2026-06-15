c41edf25dc4f018aea3804084e553a5383a66ad1
Shirasaka Ren <ren@shirasaka.ren>
2026-06-15 22:55:00 +0700

    tambah resource.tf: import state gas

--- examples/resources/biznetgio_neolite_snapshot/resource.tf
+++ examples/resources/biznetgio_neolite_snapshot/resource.tf
@@ -1,6 +1,21 @@
-# wip 0
-# wip 185
-# wip 279
-# wip 317
-# wip 527
-# wip 587
+resource "biznetgio_neolite_keypair" "main" {
+  name = "neo-lite-key"
+}
+
+resource "biznetgio_neolite_vm" "source" {
+  ssh_and_console_user = "admin"
+  console_password     = "s3cretP4ssw0rd"
+  vm_name              = "neo-lite-source"
+  product_id           = 123
+  select_os            = "ubuntu-22"
+  keypair_id           = biznetgio_neolite_keypair.main.keypair_id
+  cycle                = "m"
+}
+
+# Snapshot akun VM; snapshot punya account id sendiri.
+resource "biznetgio_neolite_snapshot" "main" {
+  neolite_account_id = biznetgio_neolite_vm.source.id
+  name               = "pre-update"
+  description        = "backup sebelum upgrade package"
+  cycle              = "m"
+}
