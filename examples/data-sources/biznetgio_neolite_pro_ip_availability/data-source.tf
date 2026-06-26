9b5df20d2ce7c22e6befca7393d4fe22bd766f0d
Shirasaka Ren <ren@shirasaka.ren>
2026-06-26 09:13:00 +0700

    tambah data-source.tf: polling biar nunggu active wkwk

--- examples/data-sources/biznetgio_neolite_pro_ip_availability/data-source.tf
+++ examples/data-sources/biznetgio_neolite_pro_ip_availability/data-source.tf
@@ -1,3 +1,8 @@
-# wip 0
-# wip 528
-# wip 705
+# Cek dulu IP available sebelum order product.
+data "biznetgio_neolite_pro_ip_availability" "this" {
+  product_id = 30
+}
+
+output "ip_available" {
+  value = data.biznetgio_neolite_pro_ip_availability.this.available
+}
