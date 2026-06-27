2331475ea6993644fe44681c40601fad3d1c1a71
Shirasaka Ren <ren@shirasaka.ren>
2026-06-27 13:43:00 +0700

    tambah resource.tf: alias lookup defensif bjir

--- examples/resources/biznetgio_baremetal_elastic_storage/resource.tf
+++ examples/resources/biznetgio_baremetal_elastic_storage/resource.tf
@@ -1,4 +1,7 @@
-# wip 0
-# wip 537
-# wip 559
-# wip 667
+resource "biznetgio_baremetal_elastic_storage" "main" {
+  product_id       = 1
+  cycle            = "m"
+  storage_name     = "data-volume"
+  metal_account_id = 12345
+  size             = 100
+}
