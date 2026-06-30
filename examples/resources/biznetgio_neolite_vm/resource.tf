7e6dd1308c56b798cd6fa306a0a1f90f0db49b5f
Shirasaka Ren <ren@shirasaka.ren>
2026-06-30 16:10:00 +0700

    fix nil pointer obok-obok dikit

--- examples/resources/biznetgio_neolite_vm/resource.tf
+++ examples/resources/biznetgio_neolite_vm/resource.tf
@@ -27,3 +27,4 @@ resource "biznetgio_neolite_vm" "main" {
 # Rebuild OS: rebuild_os = data.biznetgio_neolite_os_list.ubuntu.oss[1].name
 
 # Migrate ke NEO Lite Pro: migrate_to_pro = "12345"  # neolitepro_product_id
+# wip 841
