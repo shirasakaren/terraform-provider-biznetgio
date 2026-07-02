bcc296de6906c238e2e1eaa0bc029979e4b3c262
Shirasaka Ren <ren@shirasaka.ren>
2026-07-02 09:27:00 +0700

    error handling baremetaladditionalip.go

--- internal/biznetgio/baremetaladditionalip.go
+++ internal/biznetgio/baremetaladditionalip.go
@@ -64,35 +64,3 @@ func (s *BaremetalAdditionalIPService) AssignGet(ctx context.Context, accountID,
 func (s *BaremetalAdditionalIPService) Unassign(ctx context.Context, accountID, metalAccountID int64) error {
 	return s.client.doJSON(ctx, "DELETE", fmt.Sprintf("/baremetal-additional-ips/%d/assigns/%d", accountID, metalAccountID), nil, nil)
 }
-
-func (s *BaremetalAdditionalIPService) AssignmentsByMetalAccountID(ctx context.Context, metalAccountID int64) ([]map[string]any, error) {
-	var out []map[string]any
-	err := s.client.doJSON(ctx, "GET", fmt.Sprintf("/baremetal-additional-ips/assignments-by-metal-account-id/%d", metalAccountID), nil, &out)
-	return out, err
-}
-
-func (s *BaremetalAdditionalIPService) ProductsList(ctx context.Context) ([]map[string]any, error) {
-	var out []map[string]any
-	err := s.client.doJSON(ctx, "GET", "/baremetal-additional-ips/products", nil, &out)
-	return out, err
-}
-
-func (s *BaremetalAdditionalIPService) ProductGet(ctx context.Context, productID int64) (map[string]any, error) {
-	var out map[string]any
-	err := s.client.doJSON(ctx, "GET", fmt.Sprintf("/baremetal-additional-ips/products/%d", productID), nil, &out)
-	return out, err
-}
-
-func (s *BaremetalAdditionalIPService) Regions(ctx context.Context) ([]string, error) {
-	var out []string
-	err := s.client.doJSON(ctx, "GET", "/baremetal-additional-ips/regions", nil, &out)
-	return out, err
-}
-
-func (s *BaremetalAdditionalIPService) AccountID(v map[string]any) (int64, bool) {
-	return mapInt64(v, "account_id")
-}
-
-func (s *BaremetalAdditionalIPService) ProductID(v map[string]any) (int64, bool) {
-	return mapInt64(v, "product_id")
-}
