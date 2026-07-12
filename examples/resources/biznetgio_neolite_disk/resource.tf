744571fc5080126ed1f4f9582ca1916bc17693a7
Shirasaka Ren <ren@shirasaka.ren>
2026-07-12 11:10:00 +0700

    tambah resource.tf: satu arah migrate

--- examples/resources/biznetgio_neolite_disk/resource.tf
+++ examples/resources/biznetgio_neolite_disk/resource.tf
@@ -1,2 +1,24 @@
-# wip 0
-# wip 465
+resource "biznetgio_neolite_keypair" "main" {
+  name = "neo-lite-key"
+}
+
+resource "biznetgio_neolite_vm" "main" {
+  ssh_and_console_user = "admin"
+  console_password     = "s3cretP4ssw0rd"
+  vm_name              = "neo-lite-1"
+  product_id           = 123
+  select_os            = "ubuntu-22"
+  keypair_id           = biznetgio_neolite_keypair.main.keypair_id
+  cycle                = "m"
+}
+
+# Disk tambahan; product_id dari endpoint /neolites/disks/products.
+resource "biznetgio_neolite_disk" "main" {
+  product_id         = 456
+  cycle              = "m"
+  neolite_account_id = biznetgio_neolite_vm.main.id
+  service_name       = "data-disk"
+  size               = 100
+}
+
+# Upgrade disk: naikin `size` (increment dihitung otomatis).
