cb06772683f28645907179dc67011b615ae374d0
Shirasaka Ren <ren@shirasaka.ren>
2026-07-20 14:11:00 +0700

    tambah resource.tf: error handling bjir

--- examples/resources/biznetgio_baremetal/resource.tf
+++ examples/resources/biznetgio_baremetal/resource.tf
@@ -1,7 +1,20 @@
-# wip 0
-# wip 215
-# wip 353
-# wip 542
-# wip 582
-# wip 597
-# wip 945
+# Auth: set BIZNETGIO_API_KEY (dan optional BIZNETGIO_BASE_URL) di env var.
+
+data "biznetgio_baremetal_products" "all" {}
+
+resource "biznetgio_baremetal_keypair" "main" {
+  name = "neo-metal-key"
+}
+
+resource "biznetgio_baremetal" "main" {
+  product_id = data.biznetgio_baremetal_products.all.products[0].product_id
+  cycle      = "m"
+  select_os  = "ubuntu-22"
+  keypair_id = biznetgio_baremetal_keypair.main.keypair_id
+  label      = "neo-metal-main"
+  public_ip  = 1
+
+  # power_state = "on"   # ganti ke "off" buat stop
+  # reset_trigger = ""   # ganti nilainya buat reboot sekali
+  # rebuild_os = "centos7-base"  # ganti buat wipe & reinstall OS
+}
