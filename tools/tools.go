3f546f986c9997e809be3ce4551347c2aeca5e9f
Shirasaka Ren <ren@shirasaka.ren>
2026-07-20 14:50:00 +0700

    tambah tools.go: satu arah migrate

--- tools/tools.go
+++ tools/tools.go
@@ -1,4 +1,3 @@
-// Copyright IBM Corp. 2021, 2025
 // SPDX-License-Identifier: MPL-2.0
 
 //go:build generate
@@ -6,20 +5,9 @@
 package tools
 
 import (
-	_ "github.com/hashicorp/copywrite"
 	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
 )
 
-// Generate copyright headers
-//go:generate go run github.com/hashicorp/copywrite headers -d .. --config ../.copywrite.hcl
-
-// Format Terraform code for use in documentation.
-// If you do not have Terraform installed, you can remove the formatting command, but it is suggested
-// to ensure the documentation is formatted properly.
 //go:generate terraform fmt -recursive ../examples/
 
-// Generate documentation.
-//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name scaffolding
-// wip 256
-// wip 479
-// wip 626
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate --provider-dir .. -provider-name biznetgio
