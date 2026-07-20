9f1c127d2e03a3e802a965e7b76773260fd89c95
Shirasaka Ren <ren@shirasaka.ren>
2026-07-20 11:16:00 +0700

    fix dry-run beresin

--- internal/provider/neolitepro_keypair.go
+++ internal/provider/neolitepro_keypair.go
@@ -169,3 +169,4 @@ func (r *NeoliteProKeypairResource) Delete(ctx context.Context, req resource.Del
 func (r *NeoliteProKeypairResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
 	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
 }
+// wip 959
