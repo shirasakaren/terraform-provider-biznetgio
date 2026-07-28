f5501af22d32153e8de81a85ea6532d5ed82eedf
Shirasaka Ren <ren@shirasaka.ren>
2026-07-28 22:58:00 +0700

    tambah data-source.tf: envelope unwrap

--- examples/data-sources/biznetgio_neolite_pro_os_list/data-source.tf
+++ examples/data-sources/biznetgio_neolite_pro_os_list/data-source.tf
@@ -1,6 +1,9 @@
-# wip 0
-# wip 209
-# wip 487
-# wip 783
-# wip 843
-# wip 851
+data "biznetgio_neolite_pro_products" "all" {}
+
+data "biznetgio_neolite_pro_os_list" "this" {
+  product_id = data.biznetgio_neolite_pro_products.all.products[0].product_id
+}
+
+output "first_os" {
+  value = data.biznetgio_neolite_pro_os_list.this.oss[0].name
+}
