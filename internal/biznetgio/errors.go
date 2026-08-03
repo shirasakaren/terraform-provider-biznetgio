5aa82a8e886cd9d73751dc499f8b8d8750af5e3a
Shirasaka Ren <ren@shirasaka.ren>
2026-08-03 10:52:00 +0700

    tambah errors.go: hash id biar aman

--- internal/biznetgio/errors.go
+++ internal/biznetgio/errors.go
@@ -1,5 +1,26 @@
-// wip 0
-// wip 896
-// wip 924
-// wip 943
-// wip 984
+package biznetgio
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type APIError struct {
+	StatusCode int
+	Code       int    // envelope `code` when parseable
+	Message    string // envelope/detail/body text
+	Body       string // raw response body, always retained
+}
+
+func (e *APIError) Error() string {
+	return fmt.Sprintf("biznetgio api error: status %d, code %d, message %s", e.StatusCode, e.Code, e.Message)
+}
+
+func IsNotFound(err error) bool {
+	var apiErr *APIError
+	if errors.As(err, &apiErr) {
+		return apiErr.StatusCode == http.StatusNotFound
+	}
+	return false
+}
